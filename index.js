89eb3437354b47c9253f65f4c76d31aed612d58d
Fumikazu Fujiwara <github@ze.gs>
2017-08-13 14:34:11 +0900

    add usage

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+if (process.argv.length !== 3) {
+  let usage = `Usage: ${process.argv[0]} ${process.argv[1]} `;
+  usage += `<source.yaml>`;
+  console.error(usage);
+  process.exit(0);
+}
 
 var Yoboo = require('./lib/yoboo');
 var yb = new Yoboo(process.argv[2]);
